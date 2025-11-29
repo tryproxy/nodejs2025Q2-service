5cb4064d5e8b6ba7260658f562943dab079fd381
asdqdsa <asdqdsa@protonmail.com>
2025-11-29 10:12:46 +0400

    chore: set config for envs w/ port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from './utils/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  app.setGlobalPrefix('api');
+
+  const port = config.port;
+  await app.listen(port);
 }
 bootstrap();
